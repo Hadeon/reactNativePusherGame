062a3f81e8f0407aea211b4938cfbcb0873e0265
Hadeon <andersene909@gmail.com>
2018-10-29 15:21:39 -0700

    Added main and dependencies

---
+++ reactNativePusherClient/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert
+} from 'react-native';
+
+import Pusher from 'pusher-js/react-native';
+import shortid from 'shortid';
+import Spinner from 'react-native-spinkit';
