bb329c4be949869c41430f5e19788b65ae3e7d43
Hadeon <andersene909@gmail.com>
2018-10-30 21:17:09 -0700

    Creating Main.js and setting up connection to Pusher; Prepping for Server Auth endpoint;

--- reactNativePusherClient/components/Main.js
+++ reactNativePusherClient/components/Main.js
@@ -11,3 +11,56 @@ import {
 import Pusher from 'pusher-js/react-native';
 import shortid from 'shortid';
 import Spinner from 'react-native-spinkit';
+
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      piece: '',
+      rival_username: '',
+      is_playing: false,
+      show_prompt: false,
+      is_waiting: false,
+      is_room_creator: false
+    }
+
+    this.game_channel = null;
+    this.is_channel_binded = null;
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onPressCreateRoom = this.onPressCreateRoom.bind(this);
+    this.onPressJoinRoom = this.onPressJoinRoom.bind(this);
+    this.joinRoom = this.joinRoom.bind(this);
+    this.onCancelJoinRoom = this.onCancelJoinRoom.bind(this);
+    this.endGame = this.endGame.bind(this);
+  }
+
+  componentWillMount() {
+    this.puser = new Pusher('API KEY', {
+      authEndpoint: 'AUTH ENDPOINT',
+      cluster: 'APP CLUSTER',
+      encrypted: true
+    });
+  }
+
+   componentDidUpdate() {
+     if(this.state.is_waiting && !this.is_channel_binded){
+       this.game_channel.bind('client-joined', (data) => {
+         this.setState({
+           is_waiting: false,
+           is_playing: true,
+           rival_username: data.username
+         });
+
+         if(this.state.is_room_creator){
+           this.game_channel.trigger('client-joined', {
+             username: this.state.username
+           });
+         }
+       });
+
+       this.is_channel_binded = true;
+     }
+   }
+}
